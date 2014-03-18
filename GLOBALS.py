cbec1d81d0e4905bb539a6b3a904bae3abd32829
roger <thekaratekid552>
2014-03-18 12:16:51 -0400

    Here you go

---
+++ GLOBALS.py
@@ -0,0 +1,19 @@
+#Globals: Here is where I store everything that needs to be accessed anywhere.
+
+
+class Globals():
+    VersionNumber = "v1.2"
+    Version = VersionNumber+" ~ Codename: 'Fellowship of the Hack'"
+    OpenRomName = None
+    OpenRomID = None
+    INI = None
+    PokeNames = None
+    latestRelease = None
+    """COMPILERS- Chose the one you want by making it the only one without a #."""
+    #ZIPName = "Gen_III_Suite.Windows-.English.Encoding.zip"
+    #ZIPName = "Gen_III_Suite.Windows-.Chinese.Encoding.zip"
+    #ZIPName = "Gen_III_Suite.MAC.zip"
+    ZIPName = "Gen_III_Suite.Ubuntu.13.10.zip"
+    DownloadedZipName = None
+    
+        
