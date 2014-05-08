f3a15f6368823a88c85b592eeffd3475c2ff9a93
aiudirog <aiudirog@gilbertschool.org>
2014-05-08 12:52:24 -0400

    Update MainFrame.py

--- GoogleVoiceLinuxClient/lib/UI/MainFrame.py
+++ GoogleVoiceLinuxClient/lib/UI/MainFrame.py
@@ -1,9 +1,11 @@
 import wx
+from lib.UI.MsgsPanel import *
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, 
                           size=(300,wx.GetDisplaySize()[1]))
+        Panel = MsgsPanel(self)
         
         self.Show()
 
